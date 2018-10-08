8137ae1d0dedc251ebdf01acde8d8317981941ad
RichardGTech <richardgtech1990@gmail.com>
2018-10-08 14:35:46 -0700

    Adding all JavaScript Projects from Colt Steele Course to repository

---
+++ JSStalker.js
@@ -0,0 +1,8 @@
+var FirstName = prompt("What is your name?");
+var LastName = prompt("What is your name?");
+
+var Age = prompt("How old are you?");
+
+var FullName = FirstName + " " + LastName;
+console.log("Your full name is " + FullName);
+console.log("Your age is " + Age + " years old");
---
+++ annoy.js
@@ -0,0 +1,20 @@
+//var answer = prompt("DO U NO DE WAE?");
+
+//while(answer !== "yes" && answer !== "yeah") {
+//	var answer = prompt("DO U NO DE WAE?");	
+//}
+
+//	alert("CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK");
+
+//else {
+//	var answer = prompt("DEN I SPIT ON YOU!");
+//}
+
+//V2
+var answer = prompt("DO U NO DE WAE?");
+
+while(answer.indexOf("yes") === -1) {
+	var answer = prompt("DO U NO DE WAE?");	
+}
+
+	alert("CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK")
